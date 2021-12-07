aa124c3dced183bc6aef35ec272d2b9cee77b024
Harmeet Gill <gill-h47@webmail.uwinnipeg.ca>
2021-12-07 10:06:41 -0600

    Part One Option A Complete

--- Employee_Driver.java
+++ Employee_Driver.java
@@ -4,75 +4,75 @@ import java.io.*;
 import java.text.NumberFormat;
 
 /** ***************************************************
- *  Name:           Sveinson
+ *  Name:           Harmeet Gill
  *  Class:          CS30S
  * 
- *  Assignment:     Ax Qy
+ *  Assignment:     December Exam Payroll
  * 
  *  Description:    Place a short description of your program here
  * 
  *************************************************************/
 
-public class ClientTemplate {
+public class Employee_Driver {
 
     public static void main(String[] args) throws IOException{
-    // ***** constants *******
-    
-    // ***** variables *****
-    
+        // ***** constants *******
+
+        // ***** variables *****
+
         String banner = "";             // output banner
         String prompt = "";             // prompt for user input
-        
+
         String strin = "";              // string fro keyboard input
         String strout = "";             // string for formatted output
-        
+
         String delim = "[ ]+";          // delimiter for splitting input records
         String[] tokens = null;         // used to split input records
-        
+
         // a new line character that works on every computer system
         String nl = System.lineSeparator();
-    
-    // ***** objects *****
-    
+
+        // ***** objects *****
+
         //Scanner scanner = new Scanner(System.in);
         //NumberFormat currency = NumberFormat.getCurrencyInstance();
-        
+
         // file io buffers for reading and writing to text files
-        
-        //BufferedReader fin = new BufferedReader(new FileReader("filename.txt"));
+
+        BufferedReader fin = new BufferedReader(new FileReader("EmployeeDecemberExamData.txt"));
         //PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter("outfle.txt")));
-    
-    // ***** print banners *****
-    
+
+        // ***** print banners *****
+
         banner = "*****************************" + nl;
-        banner += "Name:        Sveinson" + nl;
+        banner += "Name:        Harmeet Gill" + nl;
         banner += "Class:       CS30S" + nl;
-        banner += "Assignment:  Ax Qy" + nl;
+        banner += "Assignment:  December Exam Payroll" + nl;
         banner += "*****************************" + nl + nl;
-        
+
         System.out.println(banner);
         //fout.print(banner);
-    
-    // ***** Get Input *****
-    
+
+        // ***** Get Input *****
+
         // prompt for input
         // read input from keyboard
         // echo input back to console window
-    
-    // ***** Main Processing *****
-    
-    // ***** Print Formatted Output *****
-    
-    // ***** Closing Message *****
-    
+
+        // ***** Main Processing *****
+
+        // ***** Print Formatted Output *****
+
+        // ***** Closing Message *****
+
         System.out.println();
         System.out.println("end of processing");
         //fout.println("End of Processing");
-        
-    // **** close io buffers *****
-    
+
+        // **** close io buffers *****
+
         //fin.close();
         //fout.close();
     } // end main 
-    
+
 } // end FormatTemplate
